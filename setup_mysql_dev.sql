8fb267c8a05d7945f9c264f2ace3a85c9f6440e1
I-yan-u <iyanuajimobi12@gmail.com>
2023-03-28 23:30:36 +0100

    Create db and users

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,7 +2,8 @@
 -- creates a new user with passwd
 -- grant permissions and privilege to the user
 
-CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
-CREATE USER IF NOT EXISTS `hbnb_dev`@`localhost` IDENTIFIED BY `hbnb_dev_pwd`;
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO `hbnb_dev`@`localhost`;
-GRANT SELECT ON performance_schema.* TO `hbnb_dev`@`localhost`;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
---
+++ setup_mysql_test.sql
@@ -0,0 +1,9 @@
+-- An SQL script that creates a db
+-- creates a *TEST*user with passwd
+-- grant permissions and privilege to the *TEST*user
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
