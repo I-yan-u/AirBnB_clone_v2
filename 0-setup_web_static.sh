cefbc8aee02a0316b2ddd7ef0c7ae0da948fd419
I-yan-u <iyanuajimobi12@gmail.com>
2023-04-15 15:37:32 +0100

    test...

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,13 @@
 #!/usr/bin/env bash
-# Installs Nginx if not installed
-#+ Creates folders /data/web_static/shared
-#+	and /adta/releases/test if not exists
-#+ /data/web_static/current linked to
-#+ /data/web_static/releases/test/
-#+ Creates an /data/web_static/releases/test/index.html
-#+ Configures Nginx to serve /data/web_static/current/ 
-#+	to hbnb_static
-
+# script that sets up your web servers for the deployment of web_static
 sudo apt-get -y update
-sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "Welcome to The_Masterminds home" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+touch /data/web_static/releases/test/index.html
+ln -fs /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+echo "<html><head></head><body>Success</body></html>" > /data/web_static/releases/test/index.html
+sed -i "s/# Only/\n\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/; \n\t\
+\tautoindex off;\n\t}\n \n\t# Only/" /etc/nginx/sites-available/default
+sudo service nginx restart
