9c9918828fd46d9c2afc192b3c529c10142c8077
I-yan-u <iyanuajimobi12@gmail.com>
2023-04-15 15:14:43 +0100

    nginx setup for static files

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,7 +15,7 @@ sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-echo "Yay!!!!" | sudo tee /data/web_static/releases/test/index.html
+echo "<html><head></head><body>Yay!!!</body></html>" | sudo tee /data/web_static/releases/test/index.html
 sudo sed -i -e '$i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/; \n\t}' /etc/nginx/sites-available/default
 
 sudo service nginx restart
