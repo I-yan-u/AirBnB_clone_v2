9523b77969460db319f5239c27e8dc60e79004e0
I-yan-u <iyanuajimobi12@gmail.com>
2023-07-09 22:34:14 +0100

    deploy web_ststic

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+"""
+Fabric local archive packer
+"""
+
+from fabric import local
+from datetime import datetime
+from os.path import isdir
+
+def do_pack():
+    """
+    Pack the local archive
+    """
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir('version') is False:
+            local('mkdir version')
+        file_name = "version/web_static_{}.tgz".format(date)
+        local('tar -cvzf {} web_static'.format(file_name))
+        return file_name
+    except:
+        return None
