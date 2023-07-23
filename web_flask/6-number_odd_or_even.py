43987ab7ae4c2ea7651b2a8a9ef8d8f3d04d9892
I-yan-u <iyanuajimobi12@gmail.com>
2023-07-23 23:08:33 +0100

    conditional statements with jinja2

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+""" Flask web app """
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_routes(text):
+    """ C is ... """
+    out = "C "
+    for char in text:
+        if char == "_":
+            out += " "
+        else:
+            out += char
+    return out
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_routes(text="is cool"):
+    """ Python is cool """
+    out = "Python "
+    for char in text:
+        if char == "_":
+            out += " "
+        else:
+            out += char
+    return out
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """ number in root """
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """ Loads template HTML in n is int """
+    return render_template('5-number.html', number=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """ Displays equality along with the number """
+    return render_template('6-number_odd_or_even.html', number=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
